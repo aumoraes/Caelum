1823f1a608d2045eea8350b9bc321f293a7ba753
aumoraes <aumoraes@sp.r7.com>
2017-06-12 09:43:30 -0300

    removendo itens desnecessarios

--- caelum-fj21/src/FabricaDeCarroSingletonData.java
+++
@@ -1,7 +0,0 @@
-
-public class FabricaDeCarroSingletonData {
-	public String nome;
-
-	public FabricaDeCarroSingletonData() {
-	}
-}
--- caelum-fj21/src/HerencaPolimorfismo/ContaInvestimento.java
+++
@@ -1,8 +0,0 @@
-package HerencaPolimorfismo;
-
-public class ContaInvestimento extends Conta{
-	@Override
-	public void atualiza(double taxaSelic) {
-		this.saldo += this.saldo * taxaSelic;	
-	}
-}
